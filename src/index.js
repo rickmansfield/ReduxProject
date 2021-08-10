f82f782049d3624720ecc116423b3af015800407
Rick <rmansfield@realpeoplesratings.com>
2021-08-10 14:56:14 -0500

    Link App to Redux with createStore & Provider

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ import reducer from './reducers';
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+const store = createStore(reducer);
+console.log("STORE",store.getState());
 
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
       <App />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
+//In index.js, make use of the createStore method and Provider component to link your App to redux.
