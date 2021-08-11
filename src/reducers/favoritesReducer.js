72e074d1472ec8af1c2742276e12593104ef6061
Rick <rmansfield@realpeoplesratings.com>
2021-08-10 20:37:43 -0500

    17-19 A) completed in favoritesReducer.js

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -10,6 +10,26 @@ const initialState = {
 //** 12 B [ ] As a start, only add a default case to the switch statement. (back in favoritesReducer.js)
 const reducer = (state = initialState, action) => {
     switch(action.type){
+        //17 A) Add in a case for TOGGLE_FAVORITES
+        case TOGGLE_FAVORITES:
+            return {
+                ...state, 
+                displayFavorites: !state.displayFavorites
+            }
+        //18 A) Add in a case for ADD_FAVORITE    
+        case ADD_FAVORITE:
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            }  
+        //19 A) Add in a case for REMOVE_FAVORITE     
+        case REMOVE_FAVORITE:
+            return {
+                ...state,
+                favorites: state.favorites.filter(item =>{
+                    return item.id !== action.payload
+                })
+            }    
         default:
         return state;
     }
